11a4a234ce5dff12437c8626a8b489da75dba415
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-01-23 17:35:21 -0500

    arrow commands

--- src/index.js
+++ src/index.js
@@ -26,3 +26,7 @@ function keydownCommand ($el, key) {
 }
 
 Cypress.addChildCommand('keydown', keydownCommand)
+Cypress.addChildCommand('left', $el => keydownCommand($el, 'ArrowLeft'))
+Cypress.addChildCommand('right', $el => keydownCommand($el, 'ArrowRight'))
+Cypress.addChildCommand('up', $el => keydownCommand($el, 'ArrowUp'))
+Cypress.addChildCommand('down', $el => keydownCommand($el, 'ArrowDown'))
